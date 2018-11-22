e9219df24e02751c0745657702fbbd6625038229
Pasindu Senanayake <pasinduath@gmail.com>
2018-11-22 21:41:16 +0530

    Solve Maven Errors

---
+++ algo/src/main/java/org/neo4j/graphalgo/impl/AverageDegreeCal.java
@@ -0,0 +1,84 @@
+package org.neo4j.graphalgo.impl;
+
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.MetricRegistry;
+import org.neo4j.graphalgo.api.Graph;
+import org.neo4j.graphalgo.core.utils.ParallelUtil;
+import org.neo4j.graphalgo.core.utils.Pools;
+import org.neo4j.graphdb.Direction;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AverageDegreeCal extends Algorithm<AverageDegreeCal> {
+    private final int nodeCount;
+    private Direction direction;
+    private Graph graph;
+    private final ExecutorService executor;
+    private final int concurrency;
+    private volatile AtomicInteger nodeQueue = new AtomicInteger();
+    private final Histogram histogram;
+
+    public AverageDegreeCal(
+            Graph graph,
+            ExecutorService executor,
+            int concurrency,
+            Direction direction
+    ) {
+        if (concurrency <= 0) {
+            concurrency = Pools.DEFAULT_QUEUE_SIZE;
+        }
+
+        this.graph = graph;
+        this.executor = executor;
+        this.concurrency = concurrency;
+        nodeCount = Math.toIntExact(graph.nodeCount());
+        this.direction = direction;
+        MetricRegistry doubleRecorder = new MetricRegistry();
+        this.histogram = doubleRecorder.histogram("stats");
+    }
+
+    public AverageDegreeCal compute() {
+        nodeQueue.set(0);
+
+        List<Runnable> tasks = new ArrayList<>();
+        for (int i = 0; i < concurrency; i++) {
+            tasks.add(new DegreeTask());
+        }
+        ParallelUtil.runWithConcurrency(concurrency, tasks, executor);
+
+        return this;
+    }
+    @Override
+    public AverageDegreeCal me() {
+        return this;
+    }
+
+    @Override
+    public AverageDegreeCal release() {
+        graph = null;
+        return null;
+    }
+
+
+    private class DegreeTask implements Runnable {
+        @Override
+        public void run() {
+            for (; ; ) {
+                final int nodeId = nodeQueue.getAndIncrement();
+                if (nodeId >= nodeCount || !running()) {
+                    return;
+                }
+
+                int degree = graph.degree(nodeId, direction);
+                histogram.update(degree);
+            }
+        }
+    }
+
+    public double average() {
+        return histogram.getSnapshot().getMean();
+    }
+}
