5ea8d4fd778c76590833edae78ada0f0bb0a01c0
piyumi <rameshkafox@gmail.com>
2018-11-23 09:58:04 +0530

    add article rank result

--- algo/src/main/java/org/neo4j/graphalgo/impl/ArticleRankResult.java
+++ algo/src/main/java/org/neo4j/graphalgo/impl/ArticleRankResult.java
@@ -1,4 +1,12 @@
 package org.neo4j.graphalgo.impl;
 
+import org.neo4j.graphalgo.core.write.Exporter;
+
 public interface ArticleRankResult {
+
+    double score(int nodeId);
+
+    double score(long nodeId);
+
+    void export(String propertyName, Exporter exporter);
 }
