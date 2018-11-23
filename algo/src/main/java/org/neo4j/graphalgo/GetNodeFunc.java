588454a05d4c236bb12fc57af49288bd37b9d5fb
piyumi <rameshkafox@gmail.com>
2018-11-23 09:55:56 +0530

    Add node function

--- algo/src/main/java/org/neo4j/graphalgo/GetNodeFunc.java
+++ algo/src/main/java/org/neo4j/graphalgo/GetNodeFunc.java
@@ -1,4 +1,26 @@
 package org.neo4j.graphalgo;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class GetNodeFunc {
+    @Context
+    public GraphDatabaseAPI api;
+
+    @UserFunction("algo.getNodeById")
+    @Description("CALL algo.getNodeById(value) - return node for nodeId. null if none exists")
+    public Node getNodeById(@Name(value = "nodeId") Number nodeId) {
+        try {
+            return api.getNodeById(nodeId.longValue());
+        } catch (NotFoundException e) {
+            return null;
+        }
+    }
+
+    @UserFunction("algo.getNodesById")
+    @Description("CALL algo.getNodesById(values) - return node for nodeIds. empty if none exists")
+    public List<Node> getNodesById(@Name(value = "nodeIds") List<Number> nodeIds) {
+        return nodeIds.stream().map(this::getNodeById).filter(Objects::nonNull).collect(Collectors.toList());
+    }
 }
