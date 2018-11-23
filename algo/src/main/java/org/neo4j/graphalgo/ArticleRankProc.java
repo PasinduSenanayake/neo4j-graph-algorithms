0f2d038578b108bf178459daa5004a601f0cef53
piyumi <rameshkafox@gmail.com>
2018-11-23 09:56:19 +0530

    Update Proc

--- algo/src/main/java/org/neo4j/graphalgo/ArticleRankProc.java
+++ algo/src/main/java/org/neo4j/graphalgo/ArticleRankProc.java
@@ -1,4 +1,91 @@
 package org.neo4j.graphalgo;
 
+import org.neo4j.graphalgo.api.Graph;
+import org.neo4j.graphalgo.core.ProcedureConfiguration;
+import org.neo4j.graphalgo.core.utils.Pools;
+import org.neo4j.graphalgo.core.utils.ProgressTimer;
+import org.neo4j.graphalgo.core.utils.TerminationFlag;
+import org.neo4j.graphalgo.core.utils.paged.AllocationTracker;
+import org.neo4j.graphalgo.core.write.Exporter;
+import org.neo4j.graphalgo.impl.Algorithm;
+import org.neo4j.graphalgo.impl.ArticleRankAlgorithm;
+import org.neo4j.graphalgo.impl.ArticleRankResult;
+import org.neo4j.graphalgo.results.ArticleRankScore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.LongStream;
+
 public class ArticleRankProc {
+
+
+
+
+
+    private ArticleRankResult evaluate(
+            Graph graph,
+            AllocationTracker tracker,
+            TerminationFlag terminationFlag,
+            ProcedureConfiguration configuration,
+            ArticleRankScore.Stats.Builder statsBuilder) {
+
+        double dampingFactor = configuration.get(CONFIG_DAMPING, DEFAULT_DAMPING);
+        int iterations = configuration.getIterations(DEFAULT_ITERATIONS);
+        final int batchSize = configuration.getBatchSize();
+        final int concurrency = configuration.getConcurrency(Pools.getNoThreadsInDefaultPool());
+        log.debug("Computing article rank with damping of " + dampingFactor + " and " + iterations + " iterations.");
+
+
+        List<Node> sourceNodes = configuration.get("sourceNodes", new ArrayList<>());
+        LongStream sourceNodeIds = sourceNodes.stream().mapToLong(Node::getId);
+
+        ArticleRankAlgorithm prAlgo = ArticleRankAlgorithm.of(
+                tracker,
+                graph,
+                dampingFactor,
+                Pools.DEFAULT,
+                concurrency,
+                batchSize);
+        Algorithm<?> algo = prAlgo
+                .algorithm()
+                .withLog(log)
+                .withTerminationFlag(terminationFlag);
+
+        statsBuilder.timeEval(() -> prAlgo.compute(iterations));
+
+        statsBuilder
+                .withIterations(iterations)
+                .withDampingFactor(dampingFactor);
+
+        final ArticleRankResult articleRank = prAlgo.result();
+        algo.release();
+        graph.release();
+        return articleRank;
+    }
+
+    private void write(
+            Graph graph,
+            TerminationFlag terminationFlag,
+            ArticleRankResult result,
+            ProcedureConfiguration configuration,
+            final ArticleRankScore.Stats.Builder statsBuilder) {
+        if (configuration.isWriteFlag(true)) {
+            log.debug("Writing Article Rank results");
+            String propertyName = configuration.getWriteProperty(DEFAULT_SCORE_PROPERTY);
+            try (ProgressTimer timer = statsBuilder.timeWrite()) {
+                Exporter exporter = Exporter
+                        .of(api, graph)
+                        .withLog(log)
+                        .parallel(Pools.DEFAULT, configuration.getConcurrency(), terminationFlag)
+                        .build();
+                result.export(propertyName, exporter);
+            }
+            statsBuilder
+                    .withWrite(true)
+                    .withProperty(propertyName);
+        } else {
+            statsBuilder.withWrite(false);
+        }
+    }
+
 }
